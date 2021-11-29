a6070553676508e82c271cf7c40afacc51070f8e
Mostafa Fazli <MosFazli@gmail.com>
2021-11-29 09:47:09 +0330

    Added Hadse Adad Question
    29 November 2021
    13:18

---
+++ Race/Hadse Adad/HadseAdad.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for(int i = 0 ; i < n ; i++){
+        cin >>a[i];
+    }
+    int sum = 0;
+    for(int j = 1 ; j <= 1000 ; j++){
+        int flag = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(j % a[i] == 0){
+                flag++;
+            }
+        }
+        if(flag == n){
+            sum++;
+        }
+    }
+
+    cout <<sum ;
+}
