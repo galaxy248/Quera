ea90948b5ef11281afbdee2c5b7ee8fca2282789
Mostafa Fazli <MosFazli@gmail.com>
2021-12-02 20:08:32 +0330

    Modified Jamee Bozoragan and Esm Ha name and Added Soal Zard Question
    2 December 2021
    20:09

---
+++ Race/Soal Zard/SoalZard.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    printf("W");
+    for(i=1;i<=n;i++){
+        printf("o");
+    }
+    printf("w!");
+}
