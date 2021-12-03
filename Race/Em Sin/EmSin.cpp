3ab1adb5a66bd092a06c3cde0754c42117040ab4
Mostafa Fazli <MosFazli@gmail.com>
2021-12-03 21:23:31 +0330

    Added Em Sin Question
    3 December 2021
    21:23

---
+++ Race/Em Sin/EmSin.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int m;
+    cin>>m;
+    string str[7] ={ "sib" , "samanoo" , "sir" , "senjed" , "sabzi" , "sekke" , "saat" };
+    for(int i = 0 ; i< m ; i++){
+        cout<<str[i]<<endl;
+    }
+
+}
