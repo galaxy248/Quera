2f5039f66ec7397804d044c4b3d018082ab02e5e
Mostafa Fazli <MosFazli@gmail.com>
2021-12-04 00:11:10 +0330

    Added To Cheghadr Ezafeh Vazn Dari Question
    4 December 2021
    12:11

---
+++ Race/To Cheghadr Ezafeh Vazn Dari/ToCheghadrEzafehVaznDari.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    double m , bmi =0;
+    cin>>n>>m;
+    bmi=(n/(m*m));
+    printf("%.2f\n",bmi);
+
+    if(bmi<18.5){
+        cout<<"Underweight";
+        return 0;
+    }
+    if(bmi>= 18.5 && bmi<25){
+        cout<<"Normal";
+        return 0;
+    }
+    if(bmi>=25 && bmi <30){
+        cout<<"Overweight";
+        return 0;
+    }
+    if(bmi>=30){
+        cout<<"Obese";
+        return 0;
+    }
+}
