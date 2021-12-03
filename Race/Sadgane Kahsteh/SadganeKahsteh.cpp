adca46942c838d7f6be4ae0fe6b2bf3085ad0395
Mostafa Fazli <MosFazli@gmail.com>
2021-12-03 20:25:34 +0330

    Added Sadgane Kahsteh Question
    3 December 2021
    20:26

---
+++ Race/Sadgane Kahsteh/SadganeKahsteh.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c=0,d=0,e,f;
+    cin>>a>>b;
+    e=a;
+    f=b;
+     while (a > 0) {
+     int lastDigit = a%10;
+     c = c*10 + lastDigit;
+     a /= 10;
+     }
+     while (b > 0) {
+     int lastDigit = b%10;
+     d = d*10 + lastDigit;
+     b /= 10;
+     }
+     if(d>c){
+         //421 < 123
+         cout<<e<<" < "<<f;
+     }
+     if(d<c){
+         cout<<f<<" < "<<e;
+     }
+     if(c==d){
+         cout<<f<<" = "<<e;
+     }
+
+
+}
