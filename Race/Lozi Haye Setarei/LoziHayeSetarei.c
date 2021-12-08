ab52a2b914585ed7268ea8f647b831476778cf6a
Mostafa Fazli <MosFazli@gmail.com>
2021-12-08 08:32:43 +0330

    Added Lozi Haye Setarei Question
    8 December 2021
    08:33

---
+++ Race/Lozi Haye Setarei/LoziHayeSetarei.c
@@ -0,0 +1,35 @@
+# include <stdio.h>
+
+int main() {
+int n,i,space;
+scanf("%d",&i);
+n=(i-1)/2;
+while(n>0) {
+printf("\n");
+for(space=0;space<n;space++)
+printf(" ");
+for(space=0;space<i-n*2;space++)
+printf("*");
+for(space=0;space<n;space++)
+    printf("  ");
+for(space=0;space<i-n*2;space++)
+printf("*");
+n--;
+}
+printf("\n");
+for(space=0;space<i;space++)
+printf("**");
+n=i-2;
+while(n>0) {
+printf("\n");
+for(space=0;space<(i-n)/2;space++)
+printf(" ");
+for(space=0;space<n;space++)
+printf("*");
+for(space=0;space<(i-n)/2;space++)
+printf("  ");
+for(space=0;space<n;space++)
+printf("*");
+n=n-2;
+}
+}
